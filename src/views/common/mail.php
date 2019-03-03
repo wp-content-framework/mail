a1c70c554954aaec7e2c3b6626cf3e29b04a75ab
technote <technote.space@gmail.com>
2019-03-04 03:28:37 +0900

    fixed: author

--- src/views/common/mail.php
+++ src/views/common/mail.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * WP_Framework_Core Views Common Mail
+ * WP_Framework_Mail Views Common Mail
  *
  * @version 0.0.1
- * @author technote-space
- * @copyright technote-space All Rights Reserved
+ * @author Technote
+ * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
  * @link https://technote.space/
  */
